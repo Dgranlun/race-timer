d09081cfcfa724c794047b6fb36c09ffd1b97532
Dgranlun <dgranlun@gmail.com>
2022-10-19 16:50:24 -0400

    Update Race
    Attempted to update race to move contestants

--- src/components/Race.jsx
+++ src/components/Race.jsx
@@ -1,11 +1,14 @@
-import Contestant from './Contestant.jsx';
-import { contestantStyles } from '../styles';
-import { contestants } from '../assets/data';
+import Contestant from "./Contestant.jsx";
+import { contestantStyles } from "../styles";
+import { contestants } from "../assets/data";
+import { useState } from "react";
 
-function Race() {
+function Race(props) {
+  const [allContestants, setAllContestants] = useState(contestants);
   return (
     <div style={contestantStyles.contestantList}>
-      {contestants.map((contestant) => (
+      {props.currentTime}
+      {allContestants.map((contestant) => (
         <Contestant
           key={contestant.id}
           name={contestant.name}
